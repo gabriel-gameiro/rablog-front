b80c20dc580f562873cb3d38587f7ce96da6e50f
Gabriel Gameiro <gabriel.perez@bandtec.com.br>
2021-09-23 20:47:35 -0300

    Voltando como path ;-;

--- rotas.js
+++ rotas.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = (app) => {
 
     app.get('/', (req, res) => {
-        res.sendFile('./index.html');
+        res.sendFile(path.join(__dirname+'/index.html'));
     });
 
     app.get('/teste', (req, res) => {
