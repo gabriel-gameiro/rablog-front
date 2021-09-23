2a848e1edf9b92b84a931c1c0fa3ef9fbce9eb96
Gabriel Gameiro <gabriel.perez@bandtec.com.br>
2021-09-23 20:27:35 -0300

    removendo papagaiada de path

--- rotas.js
+++ rotas.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = (app) => {
 
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname+'/index.html'));
+        res.sendFile('./index.html');
     });
 
     app.get('/teste', (req, res) => {
